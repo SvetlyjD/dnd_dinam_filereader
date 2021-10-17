59a8fbf4d162f97874d615a4974f5fa7f3ab15cf
svetlyid <svetlyid@yandex.ru>
2021-10-17 14:24:14 +0300

    dnd_yes_not_loader

--- script.js
+++ script.js
@@ -6,14 +6,27 @@ const triggerInput = () => input.click();
 const changeHandler = (e) => {
 
     const files = e.target.files;
-    Array.from(files).forEach(file => {
+    Array.from(files).forEach((file, i) => {
         const reader = new FileReader();
 
-        reader.onprogress = event => {
+        reader.onloadstart = event => {
+            let div = document.createElement("div");
+            let preview_image = document.createElement("div");
+            preview_image.classList.add("preview-image")
+            // div.classList.add("loader");
+            // preview_image.appendChild(div);
+            // preview.appendChild(preview_image);
+        }
+        reader.onprogress = e => {
+            //     const loader = document.querySelectorAll(".loader");
+            //     let downloadPr = parseInt(e.loaded / e.total * 100);
+            //     loader[i].style.width = downloadPr + "%";
 
         }
 
         reader.onload = event => {
+            // const loader = document.querySelector(".loader");
+            const preview_image = document.querySelector(".preview_image");
             preview.insertAdjacentHTML("afterbegin", `
             <div class = "preview-image">
             <img src="${event.target.result}" alt  ="${event.target.result}" draggable="true"/>
@@ -26,12 +39,11 @@ const changeHandler = (e) => {
 
 function dnd() {
     let c = document.getElementsByTagName("img");
-    console.log(c);
+
     Array.from(c).forEach(item => {
-        // console.log(this);
         item.draggable = true;
         item.ondragstart = e => {
-            console.log(this);
+            console.log("dragstart");
             e.dataTransfer.setData("id", e.target.id);
             e.target.classList.add("dragging");
         }
@@ -55,8 +67,43 @@ function dnd() {
 
     })
 }
-dnd();
+
 button.addEventListener("click", triggerInput);
 input.addEventListener("change", changeHandler);
 
+let beginElem;
 
+preview.addEventListener('dragstart', handleDragStart);
+function handleDragStart(e) {
+    if (e.target.tagName == "IMG") {
+        e.dataTransfer.setData("src1", e.target.src);
+        e.target.classList.add("dragging");
+        beginElem = e.target;
+    } else return
+}
+
+preview.addEventListener('dragover', handleDragOver);
+function handleDragOver(e) {
+    if (e.target.tagName == "IMG") {
+        let old = document.querySelector('.dragoverelem');
+        old && old.classList.remove('dragoverelem')
+        e.target.classList.add('dragoverelem');
+        e.preventDefault();
+    }
+    else return;
+}
+
+preview.addEventListener("drop", handleDrop);
+function handleDrop(e) {
+    if (e.target.tagName == "IMG") {
+        let old = document.querySelector(".dragging");
+        old && old.classList.remove('dragging');
+        old = document.querySelector('.dragoverelem');
+        old && old.classList.remove('dragoverelem');
+        let v = e.target.src;
+        let sr = e.dataTransfer.getData("src1");
+        e.target.src = sr;
+        beginElem.src = v;
+    }
+    else return
+}
